adabcdfc5818ea3a88ca7728e4021e57831d096c
Jonathan Acra <jonathanacra@gmail.com>
2022-01-12 01:10:23 +0000

    finished todolist

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,59 +1,90 @@
 import React, { useState } from "react";
 
 const TodoList = () => {
-	let [count, setCount] = useState(0);
 	let [task, setTask] = useState("");
 	let [list, setList] = useState([]);
 	let [inputText, setInput] = useState("");
+	let [hoverState, setHover] = useState(undefined);
+	let [cursor, setCursor] = useState("pointer");
 
 	const handleInput = (pressedKey) => {
 		if (pressedKey.keyCode == 13) {
-			if (inputText === "") {
+			if (inputText.trim() === "") {
 				alert("Sorry, add task please.");
+				setInput("");
 			} else {
 				setTask(pressedKey.target.value);
 				setList([...list, task]);
 				setTask("");
 				setInput("");
-				setCount(count + 1);
 			}
 		}
 	};
+	const changeCursor = () => {
+		setCursor((prevState) => {
+			if (prevState === "pointer") {
+				return "default";
+			}
+			return "pointer";
+		});
+	};
 
 	return (
-		<div className="container-fluid">
-			<div className="pt-5 text-center mx-auto">
-				<h1 className="fw-light title opacity-25">Your Todo List</h1>
-			</div>
-			<div className="">
-				<div className="">
-					<div className="">
-						<div className="input-group border">
-							<input
-								className="form-control"
-								type="text"
-								value={inputText}
-								placeholder="To Do List"
-								onChange={(inputKeyPress) => {
-									setTask(inputKeyPress.target.value);
-									setInput(inputKeyPress.target.value);
-								}}
-								onKeyDown={(inputKeyPress2) =>
-									handleInput(inputKeyPress2)
-								}></input>
-						</div>
-						<div className="w-100 h-100">
-							<ul>
-								{list.map((singleTask, i) => {
-									return (
-										<li className="" key={i}>
+		<div className="mainCont">
+			<h1 className="fw-light title opacity-25">Your Todo List</h1>
+			<div className="listDiv">
+				<div className="inputBox">
+					<input
+						className="form-control"
+						type="text"
+						value={inputText}
+						placeholder="To Do List"
+						onKeyDown={(keyDown) => handleInput(keyDown)}
+						onChange={(inputKeyPress) => {
+							setTask(inputKeyPress.target.value);
+							setInput(inputKeyPress.target.value);
+						}}></input>
+				</div>
+				<div className="w-100 h-100">
+					<ul className="list-group">
+						{list.map((singleTask, i) => {
+							return (
+								<li
+									onMouseEnter={() => setHover(i)}
+									onMouseLeave={() => setHover(undefined)}
+									className="list-group-item"
+									key={i}>
+									<div className="row">
+										<div className="taskText col-10">
 											{singleTask}
-										</li>
-									);
-								})}
-							</ul>
-						</div>
-					</div>
+										</div>
+										<div
+											onMouseEnter={() => changeCursor}
+											className="col-2">
+											{i === hoverState ? (
+												<i
+													style={{ cursor: cursor }}
+													onClick={() =>
+														setList(
+															list.filter(
+																(item, p) =>
+																	p !== i
+															)
+														)
+													}
+													className="fas fa-trash fa-lg col-2"></i>
+											) : (
+												""
+											)}
+										</div>
+									</div>
+								</li>
+							);
+						})}
+						<li className="list-group-item">
+							You have {list.length} tasks remaining!
+						</li>
+					</ul>
 				</div>
 			</div>
 		</div>
